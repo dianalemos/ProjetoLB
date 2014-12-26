7d35ba6482dba605958bd76f43864c2f58334a08
Diana Lemos <demosbb@gmail.com>
2014-12-26 18:10:57 +0000

    Busca zona de sequencia
    Cria documento gbk com a zona de sequencia que queremos.
    
    ps . seq é o ficheiro que tirei diretamente do site.

--- leitura.py
+++ leitura.py
@@ -18,7 +18,7 @@ Entrez.email = "pg27658@alunos.uminho.pt"     # dizer ao NCBI quem somos
 filename = "gi_59800473.gbk"
 if not os.path.isfile(filename):
     # Downloading...
-    net_handle = Entrez.efetch(db="nucleotide",id="59800473", rettype="gb", retmode="text")
+    net_handle = Entrez.efetch(db="nucleotide",id="59800473", rettype="gb", retmode="text", seq_start="468401", seq_stop="727400")
     out_handle = open(filename, "w")
     out_handle.write(net_handle.read())
     out_handle.close()
@@ -29,19 +29,31 @@ print("Parsing...")
 record = SeqIO.read(filename, "genbank")
 print(record)
 print ""
-print ""
-
-
-# verificar as anotações correspondentes à zona definida
 print "Verificacao das anotações correspondentes à zona definida: "
 print ""
-
-Entrez.email = "pg27658@alunos.uminho.pt"
-handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id = "59800473")
-for seq_record in SeqIO.parse(handle, "gb"):
-    print seq_record.id, seq_record.description[:50] + "..."
-    print "Sequence length %i," % len(seq_record),
-    print "%i features," % len(seq_record.features),    
-    print "from: %s" % seq_record.annotations["source"]
+records = SeqIO.parse("gi_59800473.gbk", "genbank")
+for seq_record in records:
+    print "ID %s, Name %s, Description %s" % (seq_record.id, seq_record.name, seq_record.description[:50])
+    print "Sequence length: %i" % len(seq_record)
+    print "%i features" % len(seq_record.features)    
+    print "from %s" % seq_record.annotations["source"]
     print ""
-handle.close()
+
+## verificar as anotações correspondentes à zona definida
+#print "Verificacao das anotações correspondentes à zona definida: "
+#print ""
+#
+#Entrez.email = "pg27658@alunos.uminho.pt"
+#handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id = "59800473", seq_start="468401", seq_stop="727400")
+#out_handle = open("sequencia.gbk", "w")
+#out_handle.write(handle.read())
+#out_handle.close()
+#
+#records = SeqIO.parse("gi_59800473.gbk", "genbank")
+#for seq_record in records:
+#    print "ID %s, Name %s, Description %s" % (seq_record.id, seq_record.name, seq_record.description[:50])
+#    print "Sequence length: %i" % len(seq_record)
+#    print "%i features" % len(seq_record.features)    
+#    print "from %s" % seq_record.annotations["source"]
+#    print ""
+#handle.close()
