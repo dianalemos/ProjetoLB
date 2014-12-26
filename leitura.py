ce69163bc4afe41bb9652846c701a900828ee5b5
Diana Lemos <demosbb@gmail.com>
2014-12-26 19:41:49 +0000

    features

--- leitura.py
+++ leitura.py
@@ -17,7 +17,6 @@ from Bio import SeqIO
 Entrez.email = "pg27658@alunos.uminho.pt"     # dizer ao NCBI quem somos
 filename = "gi_59800473.gbk"
 if not os.path.isfile(filename):
-    # Downloading...
     net_handle = Entrez.efetch(db="nucleotide",id="59800473", rettype="gb", retmode="text", seq_start="468401", seq_stop="727400")
     out_handle = open(filename, "w")
     out_handle.write(net_handle.read())
@@ -26,8 +25,8 @@ if not os.path.isfile(filename):
     print("Saved")
 
 print("Parsing...")
-record = SeqIO.read(filename, "genbank")
-print(record)
+info = SeqIO.read(filename, "genbank")
+print(info)
 print ""
 print "Verificacao das anotações correspondentes à zona definida: "
 print ""
@@ -39,21 +38,16 @@ for seq_record in records:
     print "from %s" % seq_record.annotations["source"]
     print ""
 
-## verificar as anotações correspondentes à zona definida
-#print "Verificacao das anotações correspondentes à zona definida: "
-#print ""
-#
-#Entrez.email = "pg27658@alunos.uminho.pt"
-#handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id = "59800473", seq_start="468401", seq_stop="727400")
-#out_handle = open("sequencia.gbk", "w")
-#out_handle.write(handle.read())
-#out_handle.close()
-#
-#records = SeqIO.parse("gi_59800473.gbk", "genbank")
-#for seq_record in records:
-#    print "ID %s, Name %s, Description %s" % (seq_record.id, seq_record.name, seq_record.description[:50])
-#    print "Sequence length: %i" % len(seq_record)
-#    print "%i features" % len(seq_record.features)    
-#    print "from %s" % seq_record.annotations["source"]
-#    print ""
-#handle.close()
+features = info.features
+for aux in features:
+        if aux.type=='CDS':
+            print "Proteina codificada: %s" % aux.qualifiers['protein_id']
+            print aux
+            print ""
+        elif aux.type=='gene':
+            print aux
+            print ""
+
+        
+            
+
